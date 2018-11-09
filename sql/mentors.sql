781b16d97ca5dd43e96ef3c8dafdbcbcd621ba2d
justinchen673 <justinchenchen673@gmail.com>
2018-11-09 14:48:40 -0500

    Create mentors.sql

---
+++ sql/mentors.sql
@@ -0,0 +1,12 @@
+CREATE TABLE `mentors` (
+ `id` int unsigned NOT NULL AUTO_INCREMENT,
+ `name` varchar(100) NOT NULL,
+ `age` int unsigned NOT NULL,
+ `email` varchar(100) NOT NULL,
+ `location` varchar(100) NOT NULL,
+ `gender` varchar(100) NOT NULL,
+ `picture` varchar(100) NOT NULL,
+ `occupation` varchar(100) NOT NULL,
+ `num_mentees` int unsigned NOT NULL,
+ PRIMARY KEY (`id`)
+);
