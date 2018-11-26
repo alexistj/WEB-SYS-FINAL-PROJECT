186b06522fe3d4ec7416d810b509d7059455968f
alexistj <josepa2@rpi.edu>
2018-11-26 00:29:14 -0500

    New prototype for regis sql. Specifies Mentor and Mentee reg.

--- sql/mentees.sql
+++ sql/mentees.sql
@@ -1,11 +1,12 @@
 CREATE TABLE `mentees` (
- `id` int unsigned NOT NULL AUTO_INCREMENT,
+ `id` int unsigned NOT NULL,
  `name` varchar(100) NOT NULL,
  `age` int unsigned NOT NULL,
  `email` varchar(100) NOT NULL,
- `location` varchar(100) NOT NULL,
- `gender` varchar(100) NOT NULL,
- `picture` varchar(100) NOT NULL,
+ `location` varchar(100) ,
+ `gender` varchar(100) ,
  `password` varchar(100) NOT NULL,
- PRIMARY KEY (`id`)
+ PRIMARY KEY (`id`),
+ FOREIGN KEY (`id`) REFERENCES members(`id`)
+
 );
